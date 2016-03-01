66babb0abda2b5039b2fc9f006c9393584c76e4a
Thomas Lim <txsl@outlook.com>
2016-03-01 15:10:22 -0800

    Split Prompt into two parts - one for state, the next for the view

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -13,13 +13,13 @@ var PromptContainer = React.createClass({
 			}
 	},
 
-	onUpdateUser : function (e) {
+	handleUpdateUser : function (e) {
 		this.setState({
 			username: e.target.value
 		});
 	},
 
-	onSubmitUser: function (e) {
+	handleSubmitUser: function (e) {
 		e.preventDefault();
 		var username = this.state.username;
 		this.setState({
@@ -42,29 +42,12 @@ var PromptContainer = React.createClass({
 
 	render: function () {
 		return (
-			<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={styles.transparentBg}>
-				<h1>{this.props.route.header}</h1>
-				<div className="col-sm-12">
-					<form onSubmit={this.onSubmitUser}>
-						<div className='form-group'>
-							<input 
-								className='form-control'
-								placeholder='GitHub Username'
-								onChange={this.onUpdateUser}
-								value={this.state.username}
-								type='text'
-								/>
-						</div>
-						<div className='form-group col-sm-4 col-sm-offset-4'>
-							<button 
-								className="btn btn-block btn-success"
-								type="submit">
-									Continue
-							</button>
-						</div>
-					</form>
-				</div>
-			</div>
+			<Prompt 
+				onSubmitUser={this.handleSubmitUser}
+				onUpdateUser={this.handleUpdateUser}
+				header={this.props.route.header}
+				username={this.state.username}
+				/>
 			)
 	}
 });
